05652b1a0372ff1e898961df08965589aa8e6362
shadabhashmi21 <er.shadabhashmi@gmail.com>
2024-10-03 15:40:06 +0530

    completed: UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(final BuildContext context) => MaterialApp(
       title: 'Gapopa Assignment',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
